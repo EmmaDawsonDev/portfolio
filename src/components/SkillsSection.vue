9ae62e30f7be5a8a25d7fe2b9a8ec76384f0b733
Emma Dawson <emma.l.dawson@gmail.com>
2021-01-09 12:42:57 +0100

    added skills section

--- src/components/SkillsSection.vue
+++ src/components/SkillsSection.vue
@@ -1,7 +1,59 @@
 <template>
   <section>
     <h2>Skills</h2>
-    <section></section>
+    <section class="skills">
+      <div class="logo">
+        <img src="../assets/logos/js-img.png" alt="Javascript Logo" />
+        <p>Javascript</p>
+      </div>
+      <div class="logo">
+        <img src="../assets/logos/html-img.png" alt="HTML Logo" />
+        <p>HTML</p>
+      </div>
+      <div class="logo">
+        <img src="../assets/logos/css-img.png" alt="CSS Logo" />
+        <p>CSS</p>
+      </div>
+      <div class="logo">
+        <img src="../assets/logos/vue-img.png" alt="Vue Logo" />
+        <p>Vue</p>
+      </div>
+      <div class="logo">
+        <img src="../assets/logos/node-img.png" alt="node Logo" />
+        <p>Node</p>
+      </div>
+      <div class="logo">
+        <img src="../assets/logos/npm-img.png" alt="npm Logo" />
+        <p>NPM</p>
+      </div>
+      <div class="logo">
+        <img src="../assets/logos/vscode-img.png" alt="vscode Logo" />
+        <p>VS Code</p>
+      </div>
+      <div class="logo">
+        <img src="../assets/logos/git-img.png" alt="git Logo" />
+        <p>Git</p>
+      </div>
+      <div class="logo">
+        <img src="../assets/logos/gh-img.png" alt="Github Logo" />
+        <p>Github</p>
+      </div>
+      <div class="logo">
+        <img src="../assets/logos/figma-img.png" alt="figma Logo" />
+        <p>Figma</p>
+      </div>
+      <div class="logo">
+        <img src="../assets/logos/access-img.png" alt="Accessibility Logo" />
+        <p>Accessibility</p>
+      </div>
+      <div class="logo">
+        <img
+          src="../assets/logos/responsive-img.png"
+          alt="Responsive Design Logo"
+        />
+        <p>Responsive Design</p>
+      </div>
+    </section>
   </section>
 </template>
 
@@ -12,5 +64,23 @@ export default {};
 <style scoped>
 section {
   text-align: center;
+}
+
+.skills {
+  margin: 10px;
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+}
+
+@media (min-width: 700px) {
+  .skills {
+    margin: 5rem auto;
+    width: 80%;
+    gap: 3rem;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+  }
 }
 </style>
