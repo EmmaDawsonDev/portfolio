3940b00ef0adaedfb31e19c43cc302b337e2baec
Emma Dawson <emma.l.dawson@gmail.com>
2021-10-29 10:14:43 +0200

    Add företagspoolen to projects section

--- src/assets/projectsdata.js
+++ src/assets/projectsdata.js
@@ -1,32 +1,40 @@
 const projects = [
   {
     id: 1,
-    name: "Sinus Webshop",
-    imgURL: "sinus-webshop",
+    name: 'Företagspoolen ITHS Stockholm',
+    imgURL: 'foretagspoolen',
     desc:
-      "An e-commerce store built in Vue JS with VueX and routing. Customers can create an account to save their information and there is also an admin interface to create, read, update and delete products and orders.",
-    codelink: "https://github.com/emmalearnscode/sinus-webshop",
-    sitelink: "https://youtu.be/rikf5ZNmD6I",
+      'A searchable, sortable resource for ITHS Stockholm students to search for internships. The site is built in React and Firebase and also has a password-protected admin interface for staff to keep the internship pool updated.',
+    codelink: 'https://github.com/emmalearnscode/foretagspoolen',
+    sitelink: 'https://youtu.be/qUmTN5Ailc8',
   },
   {
     id: 2,
-    name: "Beer Wiki",
-    imgURL: "beer-wiki",
+    name: 'Sinus Webshop',
+    imgURL: 'sinus-webshop',
     desc:
-      "A single page application where you can search for a variety of beers, created using the punkIPA API, written in HTML, CSS and JavaScript. It is possible to acess a randomly chosen beer, search simply by name or do an advanced search. The site is fully mobile-responsive.",
-    codelink: "https://github.com/emmalearnscode/js10-vanilla-slutprojekt",
-    sitelink: "https://emmalearnscode.github.io/js10-vanilla-slutprojekt/",
+      'An e-commerce store built in Vue JS with VueX and routing. Customers can create an account to save their information and there is also an admin interface to create, read, update and delete products and orders.',
+    codelink: 'https://github.com/emmalearnscode/sinus-webshop',
+    sitelink: 'https://youtu.be/rikf5ZNmD6I',
   },
   {
     id: 3,
-    name: "E-wallet",
-    imgURL: "e-wallet",
+    name: 'Beer Wiki',
+    imgURL: 'beer-wiki',
     desc:
-      "Built in Vue JS, E-wallet is a single page application with the ability to add and remove credit cards, display the current active card and see all available cards. The page has two different views: the Home view where all cards are visible and the Add Card view where new cards are added and then appear in Home. All changes are seen dynamically on screen.",
-    codelink:
-      "https://github.com/emmalearnscode/iths-e-wallet/tree/main/e-wallet-project",
-    sitelink: "https://emmalearnscode.github.io/iths-e-wallet/",
+      'A single page application where you can search for a variety of beers, created using the punkIPA API, written in HTML, CSS and JavaScript. It is possible to acess a randomly chosen beer, search simply by name or do an advanced search. The site is fully mobile-responsive.',
+    codelink: 'https://github.com/emmalearnscode/js10-vanilla-slutprojekt',
+    sitelink: 'https://emmalearnscode.github.io/js10-vanilla-slutprojekt/',
   },
-];
+  {
+    id: 4,
+    name: 'E-wallet',
+    imgURL: 'e-wallet',
+    desc:
+      'Built in Vue JS, E-wallet is a single page application with the ability to add and remove credit cards, display the current active card and see all available cards. The page has two different views: the Home view where all cards are visible and the Add Card view where new cards are added and then appear in Home. All changes are seen dynamically on screen.',
+    codelink: 'https://github.com/emmalearnscode/iths-e-wallet/tree/main/e-wallet-project',
+    sitelink: 'https://emmalearnscode.github.io/iths-e-wallet/',
+  },
+]
 
 export default projects;
